70e4e9f004473e0483f8e990c7403112208f5566
root <nagaraja.hm.0001@gmail.com>
2022-01-26 11:38:52 +0100

    Update: submodule SecOC

--- LATEST/SecOC.cpp
+++ LATEST/SecOC.cpp
@@ -5,3 +5,81 @@ class_SecOC SecOC;
 FUNC(void, SECOC_CODE) class_SecOC::InitFunction(void){
 }
 
+FUNC(void, SECOC_CODE) class_SecOC::DeInitFunction(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::GetVersionInfo(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::IfTransmit(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::TpTransmit(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CancelReceive(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::IfCancelTransmit(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::TpCancelTransmit(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::ChangeParameter(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::AssociateKey(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::FreshnessValueRead(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::FreshnessValueWrite(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbIfRxIndication(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbTpRxIndication(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbIfTxConfirmation(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbTpTxConfirmation(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbTriggerTransmit(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbCopyRxData(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbCopyTxData(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CbStartOfReception(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CalloutGetRxFreshness(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CalloutGetRxFreshnessAuchData(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CalloutGetTxFreshness(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CalloutGetTxFreshnessTruncData(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::CalloutSPduTxConfirmation(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::MainFunctionRx(void){
+}
+
+FUNC(void, SECOC_CODE) class_SecOC::MainFunctionTx(void){
+}
+
--- LATEST/SecOC.h
+++ LATEST/SecOC.h
@@ -5,7 +5,33 @@
 
 class class_SecOC{
    public:
-      FUNC(void, SECOC_CODE) InitFunction(void);
+      FUNC(void, SECOC_CODE) InitFunction                   (void);
+      FUNC(void, SECOC_CODE) DeInitFunction                 (void);
+      FUNC(void, SECOC_CODE) GetVersionInfo                 (void);
+      FUNC(void, SECOC_CODE) IfTransmit                     (void);
+      FUNC(void, SECOC_CODE) TpTransmit                     (void);
+      FUNC(void, SECOC_CODE) CancelReceive                  (void);
+      FUNC(void, SECOC_CODE) IfCancelTransmit               (void);
+      FUNC(void, SECOC_CODE) TpCancelTransmit               (void);
+      FUNC(void, SECOC_CODE) ChangeParameter                (void);
+      FUNC(void, SECOC_CODE) AssociateKey                   (void);
+      FUNC(void, SECOC_CODE) FreshnessValueRead             (void);
+      FUNC(void, SECOC_CODE) FreshnessValueWrite            (void);
+      FUNC(void, SECOC_CODE) CbIfRxIndication               (void);
+      FUNC(void, SECOC_CODE) CbTpRxIndication               (void);
+      FUNC(void, SECOC_CODE) CbIfTxConfirmation             (void);
+      FUNC(void, SECOC_CODE) CbTpTxConfirmation             (void);
+      FUNC(void, SECOC_CODE) CbTriggerTransmit              (void);
+      FUNC(void, SECOC_CODE) CbCopyRxData                   (void);
+      FUNC(void, SECOC_CODE) CbCopyTxData                   (void);
+      FUNC(void, SECOC_CODE) CbStartOfReception             (void);
+      FUNC(void, SECOC_CODE) CalloutGetRxFreshness          (void);
+      FUNC(void, SECOC_CODE) CalloutGetRxFreshnessAuchData  (void);
+      FUNC(void, SECOC_CODE) CalloutGetTxFreshness          (void);
+      FUNC(void, SECOC_CODE) CalloutGetTxFreshnessTruncData (void);
+      FUNC(void, SECOC_CODE) CalloutSPduTxConfirmation      (void);
+      FUNC(void, SECOC_CODE) MainFunctionRx                 (void);
+      FUNC(void, SECOC_CODE) MainFunctionTx                 (void);
 };
 
 extern class_SecOC SecOC;
