9685085fd1d16721638925f38c4d7c053ef31180
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:08:23 +0100

    Update: submodule SecOC

--- LATEST/SecOC_EcuM.h
+++ LATEST/SecOC_EcuM.h
@@ -27,8 +27,8 @@ class interface_SecOC_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, SECOC_CODE) InitFunction   (void);
-      FUNC(void, SECOC_CODE) DeInitFunction (void);
+      virtual FUNC(void, SECOC_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, SECOC_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
