c2156931557d9338c0acaf694245792cf5482aa9
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-28 11:03:53 +0000

    Update: AUTOSAR basic software service module SecOC

--- LATEST/SecOC.cpp
+++ LATEST/SecOC.cpp
@@ -7,6 +7,7 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "Module.hpp"
+#include "SecOC_core.hpp"
 #include "infSecOC_EcuM.hpp"
 #include "infSecOC_Dcm.hpp"
 #include "infSecOC_SchM.hpp"
