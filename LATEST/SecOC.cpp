ccf4d38d3e710eee7455eb9ab27411ae7c1c4c74
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:09:51 +0000

    Update: AUTOSAR basic software service module SecOC

--- LATEST/SecOC.cpp
+++ LATEST/SecOC.cpp
@@ -89,12 +89,10 @@ FUNC(void, SECOC_CODE) module_SecOC::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgSecOC;
          }
       }
