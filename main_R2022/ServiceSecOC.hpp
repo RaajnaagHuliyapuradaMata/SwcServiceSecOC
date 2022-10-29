480d77dfb046ae79cd5fb19cc9ea079add311abd
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-29 15:51:38 +0200

    Update: AUTOSAR basic software service module for secured onboard communication stack

---
+++ main_R2022/ServiceSecOC.hpp
@@ -0,0 +1,70 @@
+#pragma once
+/******************************************************************************/
+/* File   : ServiceSecOC.hpp                                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstServiceSecOC.hpp"
+#include "CfgServiceSecOC.hpp"
+#include "ServiceSecOC_core.hpp"
+#include "infServiceSecOC_Exp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class module_ServiceSecOC:
+      INTERFACES_EXPORTED_SERVICESECOC
+      public abstract_module
+   ,  public class_ServiceSecOC_Functionality
+{
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+      const ConstServiceSecOC_Type* lptrConst = (ConstServiceSecOC_Type*)NULL_PTR;
+
+   public:
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+      FUNC(void, SERVICESECOC_CODE) InitFunction(
+            CONSTP2CONST(ConstModule_TypeAbstract, SERVICESECOC_CONST,       SERVICESECOC_APPL_CONST) lptrConstModule
+         ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICESECOC_CONFIG_DATA, SERVICESECOC_APPL_CONST) lptrCfgModule
+      );
+      FUNC(void, SERVICESECOC_CODE) DeInitFunction           (void);
+      FUNC(void, SERVICESECOC_CODE) MainFunction             (void);
+      SERVICESECOC_CORE_FUNCTIONALITIES
+
+      FUNC(void, SERVICESECOC_CODE) MainFunctionRouteSignals (void);
+      FUNC(void, SERVICESECOC_CODE) MainFunctionRx           (void);
+      FUNC(void, SERVICESECOC_CODE) MainFunctionTx           (void);
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+extern VAR(module_ServiceSecOC, SERVICESECOC_VAR) ServiceSecOC;
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ main_R2022/ServiceSecOC_core.hpp
@@ -0,0 +1,94 @@
+#pragma once
+/******************************************************************************/
+/* File   : ServiceSecOC_core.hpp                                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_ServiceSecOC.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define SERVICESECOC_CORE_FUNCTIONALITIES                                             \
+              FUNC(void, SERVICESECOC_CODE) IfTransmit                     (void);     \
+              FUNC(void, SERVICESECOC_CODE) TpTransmit                     (void);     \
+              FUNC(void, SERVICESECOC_CODE) McalCancelReceive                  (void);     \
+              FUNC(void, SERVICESECOC_CODE) IfMcalCancelTransmit               (void);     \
+              FUNC(void, SERVICESECOC_CODE) TpMcalCancelTransmit               (void);     \
+              FUNC(void, SERVICESECOC_CODE) ChangeParameter                (void);     \
+              FUNC(void, SERVICESECOC_CODE) AssociateKey                   (void);     \
+              FUNC(void, SERVICESECOC_CODE) McalFreshnessValueRead             (void);     \
+              FUNC(void, SERVICESECOC_CODE) McalFreshnessValueWrite            (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbIfRxIndication               (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbTpRxIndication               (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbIfTxConfirmation             (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbTpTxConfirmation             (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbTriggerTransmit              (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbCopyRxData                   (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbCopyTxData                   (void);     \
+              FUNC(void, SERVICESECOC_CODE) CbStartOfReception             (void);     \
+              FUNC(void, SERVICESECOC_CODE) CalloutGetRxMcalFreshness          (void);     \
+              FUNC(void, SERVICESECOC_CODE) CalloutGetRxMcalFreshnessAuchData  (void);     \
+              FUNC(void, SERVICESECOC_CODE) CalloutGetTxMcalFreshness          (void);     \
+              FUNC(void, SERVICESECOC_CODE) CalloutGetTxMcalFreshnessTruncData (void);     \
+              FUNC(void, SERVICESECOC_CODE) CalloutSPduTxConfirmation      (void);     \
+
+#define SERVICESECOC_CORE_FUNCTIONALITIES_VIRTUAL                                     \
+      virtual FUNC(void, SERVICESECOC_CODE) IfTransmit                     (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) TpTransmit                     (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) McalCancelReceive                  (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) IfMcalCancelTransmit               (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) TpMcalCancelTransmit               (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) ChangeParameter                (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) AssociateKey                   (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) McalFreshnessValueRead             (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) McalFreshnessValueWrite            (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbIfRxIndication               (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbTpRxIndication               (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbIfTxConfirmation             (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbTpTxConfirmation             (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbTriggerTransmit              (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbCopyRxData                   (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbCopyTxData                   (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CbStartOfReception             (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CalloutGetRxMcalFreshness          (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CalloutGetRxMcalFreshnessAuchData  (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CalloutGetTxMcalFreshness          (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CalloutGetTxMcalFreshnessTruncData (void) = 0; \
+      virtual FUNC(void, SERVICESECOC_CODE) CalloutSPduTxConfirmation      (void) = 0; \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_ServiceSecOC_Functionality{
+   public:
+      SERVICESECOC_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
